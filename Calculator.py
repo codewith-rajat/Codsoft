be0e27a85139189e2e24d7eeac992435bff9035b
codewith-rajat <rajatagrawal021@gmail.com>
2024-07-09 22:39:39 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter the first number: "))
+operation=input("Enter the Operator: ")
+num2=int(input("Enter the second number: "))
+if operation=="+":
+    print(num1+num2)
+elif operation=="-":
+    print(num1-num2)
+elif operation=="*":
+    print(num1*num2)
+elif operation=="/":
+    print(num1/num2)
+elif operation=="%":
+    print(num1%num2)
